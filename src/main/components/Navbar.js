d1196a66ecf8054b29577fb8bc2db47d85654272
Rektovski <murmanishvili94@gmail.com>
2022-12-07 00:05:07 +0400

    Added - Challenge 1 QR Code

---
+++ src/main/components/Navbar.js
@@ -0,0 +1,12 @@
+import "../styles/navbarStyle.css";
+
+export default function Navbar() {
+    return (
+        <>
+            <div>
+                <div className={'border border-5 border-danger'}>bitcamp challenges</div>
+
+            </div>
+        </>
+    )
+}
---
+++ src/main/components/QRCode.js
@@ -0,0 +1,21 @@
+import "../styles/QRCode.css";
+import qrlogo from "../images/image-qr-code.png";
+
+export default function QRCode() {
+    return (
+        <>
+            <div className={'QRCode'}>
+                <div className={'QRCard'}>
+                    <img className={'QRCardImage'} src={qrlogo} alt={'qrcode logo'}/>
+                    <div className={'QRHeaderText'}>
+                        Improve your front-end skills by building projects
+                    </div>
+                    <div className={'QRFooterText'}>
+                        Scan the QR code to visit Frontend Mentor and take
+                        your coding skills to the next level
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
