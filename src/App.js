9e7036d79f15330eb41a74b2e75f1f01a03bd262
Rektovski <murmanishvili94@gmail.com>
2022-12-08 21:52:58 +0400

    Added - Challenge 4 Profile Card Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./main/components/Navbar";
 import QRCode from "./main/components/challenge 1/QRCode";
 import NFTpreviewCard from "./main/components/challenge 2/NFTpreviewCard";
 import OrderSummaryComponent from "./main/components/challenge 3/OrderSummaryComponent";
+import ProfileCardComponent from "./main/components/challenge 4/ProfileCardComponent";
 
 export default function App() {
     return (
@@ -18,6 +19,10 @@ export default function App() {
 
             {/*Challenge 3*/}
             {/*<OrderSummaryComponent/>*/}
+
+            {/*Challenge 4*/}
+            <ProfileCardComponent/>
+
 
 
 
---
+++ src/main/components/challenge 4/ProfileCardComponent.js
@@ -0,0 +1,46 @@
+import "../../styles/challenge 4/profileCardComponentStyle.css"
+import mainFirstBall from "../../images/challenge 4/bg-pattern-top.svg";
+import mainSecondBall from "../../images/challenge 4/bg-pattern-bottom.svg";
+import ProfileCardBackgroundImage from "../../images/challenge 4/bg-pattern-card.svg";
+import ProfileCardImage from "../../images/challenge 4/image-victor.jpg";
+
+export default function ProfileCardComponent() {
+    return (
+        <>
+            <div className={'ProfileCardComponent'}>
+                <img className={'TopBall'} src={mainFirstBall} alt={'mainFirstBall'}/>
+                <img className={'BottomBall'} src={mainSecondBall} alt={'mainSecondBall'}/>
+                <div className={'ProfileCardSpace'}>
+                    <div className={"ProfileCard"}>
+                        <img className={"ProfileCardBackgroundImage"} src={ProfileCardBackgroundImage} alt={'ProfileCardBackgroundImage'}/>
+                        <img className={'ProfileCardImage'} src={ProfileCardImage} alt={'ProfileCardImage'}/>
+                        <div className={'pb-5'}>
+                            <div className={'ProfileHeader'}>
+                                <div className={'ProfileName'}>Victor Crest</div>
+                                <div className={'ProfileAge'}>26</div>
+                            </div>
+                            <div className={'ProfileLocation'}>
+                                London
+                            </div>
+                        </div>
+                        <hr />
+                        <div className={'ProfileFooter'}>
+                            <div className={'mx-4'}>
+                                <div className={'ProfileFooterHeader'}>80K</div>
+                                <div className={'ProfileFooterBottom'}>Followers</div>
+                            </div>
+                            <div className={'mx-4'}>
+                                <div className={'ProfileFooterHeader'}>803K</div>
+                                <div className={'ProfileFooterBottom'}>Likes</div>
+                            </div>
+                            <div className={'mx-4'}>
+                                <div className={'ProfileFooterHeader'}>1.4K</div>
+                                <div className={'ProfileFooterBottom'}>Photos</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
