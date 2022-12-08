f6f03c87981514cca3ac67e434bfb8a460b5fee0
Rektovski <murmanishvili94@gmail.com>
2022-12-09 02:16:26 +0400

    Added - Challenge 5 Skilled Elearning Landing Page

---
+++ src/main/components/challenge 5/SkilledElearningLandingPage.js
@@ -0,0 +1,81 @@
+import "../../styles/challenge 5/skilledElearningLandingPageStyle.css";
+import {Col, Container, Row} from "react-bootstrap";
+import logoDark from "../../images/challenge 5/logo-dark.svg";
+import logoLight from "../../images/challenge 5/logo-light.svg";
+import imageHero from "../../images/challenge 5/image-hero-desktop.png";
+import imageHeroMobile from "../../images/challenge 5/image-hero-mobile.png";
+import {skilledElearningCardList} from "./skilledElearningCardList";
+
+export default function SkilledElearningLandingPage() {
+    return (
+        <>
+            <div>
+                <Container>
+                    <div className={'SkilledElearningLandingPageHeader'}>
+                        <img className={'HeaderLeftLogo'} src={logoDark} alt={'logoDark'}/>
+                        <div className={'HeaderRightTextSpace'}>
+                            <div className={'HeaderRightText'}>
+                                Get Started
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"LandingPageHeader"}>
+                        <div className={"LandingPageHeaderLeft"}>
+                            <div className={'LandingPageHeaderLeftHeader'}>
+                                Maximize skill, minimize budget
+                            </div>
+                            <div className={'LandingPageHeaderLeftMiddle'}>
+                                Our modern courses across a range of in-demand skills will give you the knowledge you
+                                need to live the life you want.
+                            </div>
+                            <div className={'LandingPageHeaderLeftFooter'}>
+                                <div className={'LandingPageHeaderLeftFooterText'}>
+                                    Get Started
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"LandingPageHeaderRight"}>
+                            <img className={'LandingPageHeaderRightImage'}
+                                 src={window.innerWidth <= 1000 ? imageHeroMobile : imageHero}
+                                 alt={'imageHero'}/>
+                        </div>
+                    </div>
+
+                    <Container className={'LandingPageMiddle'}>
+                        <Row>
+                            <Col className={'firstCard g-5'} sm={12} md={6} lg={4} xl={4} xxl={4}>
+                                <div className={'firstCardText'}>
+                                    Check out our most popular courses!
+                                </div>
+                            </Col>
+                            {
+                                skilledElearningCardList.map((item, id) => (
+                                    <Col className={'nextCard g-5'} key={id} sm={12} md={6} lg={4} xl={4} xxl={4}>
+                                        <div style={{marginLeft: 15, marginRight: 15}}>
+                                            <img className={'nextCardImage'} src={item.image} alt={'cardFoto'}/>
+                                            <div className={'nextCardName'}>{item.name}</div>
+                                            <div className={'nextCardDescription'}>{item.desc}</div>
+                                            <div className={'nextCardFooter'}>Get Started</div>
+                                        </div>
+                                    </Col>
+                                ))
+                            }
+                        </Row>
+                    </Container>
+                </Container>
+                <div className={'LandingPageFooter'}>
+                    <Container className={"LandingPageFooterContainer"}>
+                        <div>
+                            <img src={logoLight} alt={'logoLight'}/>
+                        </div>
+                        <div className={'LandingPageFooterButton'}>
+                            <div className={'LandingPageFooterButtonText'}>
+                                Get Started
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/main/components/challenge 5/skilledElearningCardList.js
@@ -0,0 +1,33 @@
+import run from "../../images/challenge 5/icon-animation.svg";
+import design from "../../images/challenge 5/icon-design.svg";
+import photograpy from "../../images/challenge 5/icon-photography.svg";
+import crypto from "../../images/challenge 5/icon-crypto.svg";
+import business from "../../images/challenge 5/icon-business.svg";
+
+export const skilledElearningCardList = [
+    {
+        name: 'Animation',
+        desc: "Learn the latest animation techniques to create stunning motion design and captivate your audience.",
+        image: run
+    },
+    {
+        name: 'Design',
+        desc: "Create beautiful, usable interfaces to help shape the future of how the web looks.",
+        image: design
+    },
+    {
+        name: 'Photography',
+        desc: "Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.",
+        image: photograpy
+    },
+    {
+        name: 'Crypto',
+        desc: "All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.",
+        image: crypto
+    },
+    {
+        name: 'Business',
+        desc: "A step-by-step playbook to help you start, scale, and sustain your business without outside investment.",
+        image: business
+    },
+]
